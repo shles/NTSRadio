2fe9c04710131ec37b65d3ac789681410da23411
Artemiy <temitrix@gmail.com>
2017-11-26 15:07:47 +0300

    Initial Commit

---
+++ NTSRadio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NTSRadio
+//
+//  Created by Артмеий Шлесберг on 26/11/2017.
+//  Copyright © 2017 Shlesberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
